9d7e86cec732b96ac1ee039a9722867520e96440
Anju Thomas <anju.thomas2@cognizant.com>
2018-05-21 13:51:08 +1000

    change to test auto deploy

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <h1>FE Grizzly Test</h1>
         </p>
       </div>
     );
