0e6ec16b950282869103cb04fd5a9d3c100b0f1e
Blake Howe <blakehowe@hotmail.com>
2018-05-31 21:19:08 +1000

    refactored category state to be an array
    changed category state from object to array due to objects auto sorting by the key. arrays handle alot nicer when displaying data sorted by the server.

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -2,31 +2,34 @@ import * as types from '../actions/actionTypes';
 
 import _ from 'lodash';
 
-export function category(state = {}, action) { 
+export function category(state = [], action) { 
     console.log(action.type);
     switch (action.type) {
         case types.LOAD_CATEGORIES_SUCCESS:
-            const categories = action.data.content;
+            const catArray = action.data.content;
             const isFirst = action.data.first;
-            const catDict = _.mapKeys(action.data.content, "id");
-
-            return (!isFirst ? { ...state, ...catDict} : catDict);
+            console.log(catArray);
+            // returns a new state with the server data and may append to existing state
+            return (!isFirst ? [ ...state, ...catArray] : catArray);
             
         case types.DELETE_CATEGORY_SUCCESS:
-            // creates a new state that has the deleted category removed 
-            return _.omit(state, action.payload);
+            // returns a new state that has the deleted category removed
+            return state.filter(cat => cat.id !== action.payload);
             
         case types.EDIT_CATEGORY_SUCCESS:
-            const clone = _.cloneDeep(state) // Create a copy of state
-
-            // Return the updated cloned state
-            return _.update(clone, `${action.id}`, (cat) => {return {id:action.id, name: action.name, description: action.description, count: cat.count}});
+            // find index of category in state
+            var indexToEdit = _.findIndex(state, { id: action.id });
+            // create a new state
+            const newState = [...state];
+            // update the category in the new state using the index.
+            newState[indexToEdit].name = action.name;
+            newState[indexToEdit].description = action.description;
+            // return the new state with the updated category
+            return newState;
         
         case types.ADD_CATEGORY_SUCCESS:
-
-            return{
-                ...state, [action.id]: {id: action.id,name: action.name, description: action.description, count: action.count}
-            }
+            // returns a new state with the added category appended
+            return [...state, [action.id,action.name,action.description,action.count]]
 
         default:
             return state
