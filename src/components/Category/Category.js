f31c5df7f4811cc9faaf3f0afe43a9b80e3956de
Blake Howe <blakehowe@hotmail.com>
2018-06-06 10:44:08 +1000

    changed category api url and changed button text

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -29,7 +29,7 @@ class Category extends React.Component{
     }
     
     componentDidMount(){
-        this.props.fetchData(null);
+        this.props.fetchData();
     }
 
     fetchDataWithFilter() {
