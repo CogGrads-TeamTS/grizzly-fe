bed53cc3b0ac6ded742118f523778d989de24ee7
Blake Howe <blakehowe@hotmail.com>
2018-07-03 16:56:28 +1000

    fix auth datetime check

--- src/Auth/isAuthenticated.js
+++ src/Auth/isAuthenticated.js
@@ -1,6 +1,7 @@
 export default function isAuthenticated() {
     // Check whether the current time is past the
     // access token's expiry time
+    let current = Math.round((new Date()).getTime() / 1000);
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
+    return current < expiresAt;
 }
