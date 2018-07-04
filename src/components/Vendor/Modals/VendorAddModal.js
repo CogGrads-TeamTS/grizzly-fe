ccaac4a9a41ed14c688d665c815b92252207c426
Auphonix <matchstick_dan@hotmail.com>
2018-07-04 11:15:39 +1000

    Added vendor add validation

--- src/components/Vendor/Modals/VendorAddModal.js
+++ src/components/Vendor/Modals/VendorAddModal.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-import { Button, Modal,Form,FormGroup,Label, Col,Input, ModalHeader, ModalBody, ModalFooter, Fade } from 'reactstrap';
-import PropTypes from 'prop-types';
+import { Button, Modal, Form, FormGroup, Label, Row, Col, Input, ModalHeader, ModalBody, ModalFooter, Fade } from 'reactstrap';
+import { Field, reduxForm } from 'redux-form';
+import {
+    renderField, renderTextArea, required, maxLength40,
+    minValue0, noSpecialChars, isValidPrice, numOnly,
+    percentMax, isValidPercentage
+} from '../../common/redux_validation';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class VendorAddModal extends Component{
+class VendorAddModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -42,7 +47,7 @@ class VendorAddModal extends Component{
             pauseOnHover: true,
             draggable: true
         });
-        
+
     }
 
     toggle() {
@@ -107,61 +112,37 @@ class VendorAddModal extends Component{
             <div>
                 <Button color="success" onClick={this.toggle}>{this.props.buttonLabel}</Button>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Add Vendor</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup row>
-                                <Label sm={4}>Vendor Name : </Label>
-                                <Col sm={8}>
-                                    <Input type="text" name="name" placeholder="Add Vendor Name" onChange={this.handleVendorNameChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                            <Label sm={4}>About : </Label>
-                                <Col sm={8}>
-                                    <Input type="textarea" name="about" placeholder="About Vendor" onChange={this.handleAboutChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label sm={4}>Email : </Label>
-                                <Col sm={8}>
-                                    <Input type="email" name="email" placeholder="Email" onChange={this.handleEmailChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label sm={4}>Web Page : </Label>
-                                <Col sm={8}>
-                                    <Input type="url" name="webpage" placeholder="Web Page" onChange={this.handleWebPageChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label sm={4}>Contact : </Label>
-                                <Col sm={8}>
-                                    <Input type="number" name="contact" placeholder="Contact" onChange={this.handleContactChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label sm={4}>Address : </Label>
-                                <Col sm={8}>
-                                    <Input type="textarea" name="address" placeholder="Address" onChange={this.handleAddressChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label sm={4}>Portfolio URL : </Label>
-                                <Col sm={8}>
-                                    <Input type="url" name="portfolioURL" placeholder="Portfolio URL" onChange={this.handleportfolioURLChange} />
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.submitAndClose}>Add</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                    </ModalFooter>
+                    <form onSubmit={this.handleSubmit}>
+                        <ModalHeader toggle={this.toggle}>Add Vendor</ModalHeader>
+                        <ModalBody>
+                            <Field name="name" label="Name" type="text" component={renderField}
+                                placeholder="Add Name" className="form-fields" validate={[required, maxLength40, noSpecialChars]} />
+                            <Field name="about" label="About" type="textarea" component={renderTextArea}
+                                placeholder="Add About" className="form-fields" validate={[required]} />
+                            <Field name="email" label="Email" type="text" component={renderField}
+                                placeholder="Add Email" className="form-fields" validate={[required]} />
+                                <Field name="contact" label="Number" type="text" component={renderField}
+                                placeholder="Add Contact" className="form-fields" validate={[required]} />
+                            <Field name="webpage" label="Web Page" type="text" component={renderField}
+                                placeholder="Add Webpage" className="form-fields" />
+                            <Field name="address" label="Address" type="textarea" component={renderTextArea}
+                                placeholder="Add Address" className="form-fields" />
+                            <Field name="portfolioURL" label="Portfolio URL" type="text" component={renderField}
+                                placeholder="Add Contact" className="form-fields" />
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick={this.submitAndClose}>Add</Button>{' '}
+                        </ModalFooter>
+                    </form>
                 </Modal>
             </div>
         );
     }
 
 }
+
+VendorAddModal = reduxForm({
+    // a unique name for the form
+    form: 'addVendor'
+})(VendorAddModal)
 export default VendorAddModal
